e0f2abf96490ec3faaa8dd76f6f03993f29c84f0
John Chen <zhang.z.chen@intel.com>
2013-02-17 17:17:05 +0800

    Check vncserver process before starting up

--- start_server.sh
+++ start_server.sh
@@ -1,5 +1,7 @@
 #Please install tightvnc/turbovnc/vnc4server before starting noVNC server
 NOVNC_DIR=third_party/noVNC
-vncserver
+if ! ps -p `cat ~/.vnc/$(hostname):1.pid`; then
+    vncserver -geometry 520x700
+fi
 $NOVNC_DIR/utils/launch.sh --vnc localhost:5901 &
 node third_party/brackets/src/extensions/default/devices/EmulatorServer.js
