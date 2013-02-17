263bbbeba2f7db17389a6ef8fd0f1a11972036db
John Chen <zhang.z.chen@intel.com>
2013-02-17 17:16:11 +0800

    Safegard some variable in web_gen.sh

--- src/web_gen.sh
+++ src/web_gen.sh
@@ -5,8 +5,8 @@ web-gen -n "$1"
 cp HelloTizen/config.xml HelloTizen/icon.png . && rm -r HelloTizen
 if test -f index.html; then
     INDEX=index.html
-elif test -f $1.html; then
-    INDEX=$1.html
+elif test -f "$1.html"; then
+    INDEX="$1.html"
 else
     FILES=(*.html)
     INDEX=${FILES[0]}
