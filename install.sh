1df6ce3602a90144349df0a74cdc84dd16def35c
John Chen <zhang.z.chen@intel.com>
2013-02-17 17:15:32 +0800

    Refine install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 CUR=$(dirname $(readlink -e $0))
 SIMULATOR_DIR=$CUR/third_party/web-simulator
 git submodule update --init --recursive
-cd $SIMULATOR_DIR && sudo -E ./configure && jake 
+cd $SIMULATOR_DIR && ./configure && jake
 ln -sf $SIMULATOR_DIR/ext/brackets-extension  $CUR/third_party/brackets/src/extensions/default/web-simulator
 ln -sf $SIMULATOR_DIR/pkg/web $CUR/third_party/brackets/src/extensions/default/web-simulator/web
 
@@ -12,3 +12,4 @@ NOVNC_DIR=third_party/noVNC
 sudo apt-get install tightvncserver
 mkdir -p ~/.vnc
 cp $NOVNC_DIR/xstartup $NOVNC_DIR/start_emulator.sh ~/.vnc
+npm install connect now http-proxy
