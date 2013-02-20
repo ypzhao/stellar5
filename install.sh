1f9b21b484be449fca992e30fa44e158ef0194c0
John Chen <zhang.z.chen@intel.com>
2013-02-20 13:38:22 +0800

    Create user and disabled extension dir to avoid long delay opening project
    None existing document sometimes take a long time to error out,
    which causes the long delay of opening projects, which is after
    loading extensions. This long latency is probablyl casued by
    remote simulator.

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 CUR=$(dirname $(readlink -e $0))
 SIMULATOR_DIR=$CUR/third_party/web-simulator
+EXTENSIONS_DIR=$CUR/third_party/brackets/src/extensions
+DEF_EXT_DIR=$EXTENSIONS_DIR/default
 git submodule update --init --recursive
 cd $SIMULATOR_DIR && ./configure && jake
-ln -sf $SIMULATOR_DIR/ext/brackets-extension  $CUR/third_party/brackets/src/extensions/default/web-simulator
-ln -sf $SIMULATOR_DIR/pkg/web $CUR/third_party/brackets/src/extensions/default/web-simulator/web
+ln -sf $SIMULATOR_DIR/ext/brackets-extension  $DEF_EXT_DIR/default/web-simulator
+ln -sf $SIMULATOR_DIR/pkg/web $DEF_EXT_DIR/default/web-simulator/web
+mkdir -p $EXTENSIONS_DIR/user $EXTENSIONS_DIR/disabled
 
 cd $CUR
 # Setup vnc server
