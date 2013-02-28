62f6bcb35c1a4053b3b401316155b9323e661b45
John Chen <zhang.z.chen@intel.com>
2013-02-28 16:13:49 +0800

    Follow links when creating git hub pages

--- create_github_pages.sh
+++ create_github_pages.sh
@@ -1,5 +1,5 @@
 rm -r www/ samples/
 ./create_manifests.sh
-cp -r third_party/brackets/src www
+cp -rL third_party/brackets/src www
 cp -r third_party/brackets/samples .
 find www -name .git |xargs rm
